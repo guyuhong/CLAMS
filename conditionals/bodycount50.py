6e5edcdfd99170d931c69b84771fad7174060688
Melina.Shak <1549476674113305@mil>
2025-01-10 15:08:09 -0800

    init commit for bodyCount50 pyhton 3 upgrades

--- conditionals/bodycount50.py
+++ conditionals/bodycount50.py
@@ -1,45 +1,92 @@
-'''
+# coding=utf-8
 
-    checkBodyCount50
+#     National Oceanic and Atmospheric Administration (NOAA)
+#     Alaskan Fisheries Science Center (AFSC)
+#     Resource Assessment and Conservation Engineering (RACE)
+#     Midwater Assessment and Conservation Engineering (MACE)
 
-    checkBodyCount50 is a CLAMS validation class that checks if a body should be frozen
+#  THIS SOFTWARE AND ITS DOCUMENTATION ARE CONSIDERED TO BE IN THE PUBLIC DOMAIN
+#  AND THUS ARE AVAILABLE FOR UNRESTRICTED PUBLIC USE. THEY ARE FURNISHED "AS
+#  IS."  THE AUTHORS, THE UNITED STATES GOVERNMENT, ITS INSTRUMENTALITIES,
+#  OFFICERS, EMPLOYEES, AND AGENTS MAKE NO WARRANTY, EXPRESS OR IMPLIED,
+#  AS TO THE USEFULNESS OF THE SOFTWARE AND DOCUMENTATION FOR ANY PURPOSE.
+#  THEY ASSUME NO RESPONSIBILITY (1) FOR THE USE OF THE SOFTWARE AND
+#  DOCUMENTATION; OR (2) TO PROVIDE TECHNICAL SUPPORT TO USERS.
 
-    Validations are classes that are used by the CLAMS specimen module
-    to check
+"""
+.. module:: BodyCount50
 
+    :synopsis: BodyCount50 is a conditional that checks if a body should be frozen
 
-'''
+| Developed by:  Rick Towler   <rick.towler@noaa.gov>
+|                Kresimir Williams   <kresimir.williams@noaa.gov>
+| National Oceanic and Atmospheric Administration (NOAA)
+| National Marine Fisheries Service (NMFS)
+| Alaska Fisheries Science Center (AFSC)
+| Midwater Assesment and Conservation Engineering Group (MACE)
+|
+| Author:
+|       Rick Towler   <rick.towler@noaa.gov>
+|       Kresimir Williams   <kresimir.williams@noaa.gov>
+| Maintained by:
+|       Rick Towler   <rick.towler@noaa.gov>
+|       Kresimir Williams   <kresimir.williams@noaa.gov>
+|       Mike Levine   <mike.levine@noaa.gov>
+|       Nathan Lauffenburger   <nathan.lauffenburger@noaa.gov>
+        Melina Shak <melina.shak@noaa.gov>
+"""
 
-from PyQt4.QtCore import *
-from PyQt4 import QtSql
-from math import ceil
-
+from PyQt6.QtCore import *
+import unittest
 class BodyCount50(QObject):
 
     def __init__(self, db):
         '''
             The init methods of CLAMS conditionals are run whenever a new protocol
             or species is selected in the specimen module. Any setup that the
-            validation requires should be done here. The two input arguments are:
+            conditional requires should be done here. The three input arguments are:
 
-                db - a reference to the CLAMS QtSQLDatabase object
-                speciesCode - the
+                db - a reference to the active dbConnection class object
+
+            If you need to pass additional data to a validation, you should
+            add this data to the species_data table and query it out here in
+            the init method (see LengthRange.py for example.)
 
         '''
         #  call the superclass init
         QObject.__init__(self, None)
-        self.cnt = 0;           # initialize the counter
+        self.cnt = 0           # initialize the counter
 
 
     def evaluate(self,   measurements,  values,  result):
         '''
-            The evaluate method is called when measuremets are taken to see what changes in the protocol
+            The evaluate method is called when a measurement is taken to determine
+            what changes in the protocol. Each measurement can have from 0-N validations. When
+            a specific measurement is made, say "barcode", all validations
+            assigned to the barcode measurement will have their validate methods
+            called. Each one should verify that the currentValue is valid based
+            on the logic of each particular validation.
+
+                measurements - a list of the measurement types for this
+                    protocol, in order.
+                values - a list of the stored values of those measurements.
+                    In order of the measurements.
+                result -
+
+            For example, this conditional is for the body count measurement and when
+            a count value is logged, it will check to see if that value is greater than 50.
+
+            This is a fairly simple example, but the conditional can be
+            more complex (but usually don't need to be.) Also, remember that
+            these run each time a measurement configured for the conditional
+            runs so you don't want them to take too long to execute as it
+            will slow data collection.
+
         '''
 
-        if values[0] != None:                                 # first reading must be mandatory and completed
+        if values[0] is not None: # first reading must be mandatory and completed
             if values.count(None) == len(values)-1:   # all other readings must be None
                 self.cnt = self.cnt+1                          # increment the counter
-
 
         if self.cnt > 50:
             try:
@@ -49,3 +96,86 @@ class BodyCount50(QObject):
 
         return result
 
+
+'''
+The conditionalTest class enables testing of conditionals by creating a database
+connection, creating an instance of the conditional object, and then executing its
+evaluate method.
+
+This class will need to be customized a bit for each individual validation.
+'''
+class conditionalTest(QObject):
+        def __init__(self):
+            super(conditionalTest, self).__init__(None)
+
+            #  we use a timer here to add runTest to the event processing queue and
+            #  then exit the init. Execution will return to main, where the
+            #  application event loop will be started by the call to app.exec() which
+            #  will then start processing events on the queue which will then execute
+            #  runTest with the event loop running.
+            startTimer = QTimer(self)
+            startTimer.timeout.connect(self.runTest)
+            startTimer.setSingleShot(True)
+            startTimer.start(0)
+
+
+        def runTest(self):
+            '''runTest attempts to create a database connection by presenting a dialog
+            requesting credentials. If successful, it instantiates the validation and
+            runs the validate method of said validation. You should set up any
+            specific parameters required for this validation's test here.
+            '''
+
+            #  set up the required parameters for this test
+            measurements = ['whole_fish']
+            results = [3]
+            values = [1, None, None]
+
+            #  create a connection dialog to get connection params - by default
+            #  this will create a dbConnection object and store it in the "db"
+            #  attribute.
+            conenctionDialog = connectdlg.ConnectDlg(None, None, None)
+            ok = conenctionDialog.exec()
+
+            #  if we've connected to the database, create and run the validation
+            if ok:
+                db = conenctionDialog.db
+
+                #  create the validation using the db connection and specified species
+                #  and subcategory.
+                self.evaluate = BodyCount50(db)
+
+                #  execute the validation 55 times
+                for x in range(55):
+                    ok = self.evaluate.evaluate(measurements, values, results)
+
+                #  print the results
+                print(ok)
+
+            else:
+                print("Unable to connect to the database")
+
+            #  exit the application
+            QApplication.instance().quit()
+
+
+if __name__ == '__main__':
+    #  import test specific libraries
+    import sys
+    from pathlib import Path
+    from PyQt6.QtCore import *
+    from PyQt6.QtGui import *
+    from PyQt6.QtWidgets import *
+
+    file = Path(__file__).resolve()
+    sys.path.append(str(file.parents[1]))
+    import connectdlg
+
+    #  create an instance of QApplication
+    app = QApplication(sys.argv)
+
+    #  instantiate the test
+    form = conditionalTest()
+
+    #  and start the application event loop
+    app.exec()
